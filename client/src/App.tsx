8809b56d2cf7334586b99b78264c742fe91dec0d
Yash Ubana <yashubana789@gmail.com>
2024-04-20 17:40:24 +0530

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button";
+import { ResizablePanel } from "./components/ui/resizable";
+
+function App() {
+
+  return (
+    <>
+     
+      <Button variant="destructive">This is button</Button>
+    </>
+  );
+}
+
+export default App
